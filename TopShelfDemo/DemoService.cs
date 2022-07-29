3e8eb2d4a5d31d25e8b5cf874734bf997ac39bed
Parikshanth <parikshanth@hotmail.com>
2022-07-29 12:20:48 +0530

    Add project files.

---
+++ TopShelfDemo/DemoService.cs
@@ -0,0 +1,42 @@
+﻿using Serilog;
+using TopShelfDemo.SampleDependency;
+
+namespace TopShelfDemo
+{
+    internal class DemoService
+    {
+        private readonly ILogger _logger;
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+        private readonly ISample _sample;
+
+        public DemoService(ILogger logger, ISample sample)
+        {
+            this._logger = logger;
+            this._sample = sample;
+        }
+
+
+
+        public void Start()
+        {
+            _logger.Information("Starting the service");
+
+            Task.Run(() => { 
+            while(true)
+            {
+                _logger.Information("The service is running with sample {0}",_sample.GetDependentColour());
+                System.Threading.Thread.Sleep(3000);
+            }
+            }, _cts.Token);
+
+
+        } 
+
+        public void Stop()
+        {
+            _logger.Warning("Stopping the service");
+            _cts.Cancel();
+        }
+
+    }
+}
---
+++ TopShelfDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using Autofac;
+using AutofacSerilogIntegration;
+using Serilog;
+using Topshelf;
+using Topshelf.Autofac;
+using TopShelfDemo;
+using TopShelfDemo.SampleDependency;
+
+string loggingTemplate = "{Timestamp:G} [{Level:u3}] [{SourceContext}] {Message}{NewLine:l}{Exception:l}";
+
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .Enrich.FromLogContext()
+    .WriteTo.Console(outputTemplate: loggingTemplate)
+    .CreateLogger();
+
+var buider = new ContainerBuilder();
+buider.RegisterLogger();
+
+buider.RegisterType<DemoService>();
+
+
+//buider.RegisterType<SampleOrange>().As<ISample>();
+buider.RegisterType<SampleRed>().As<ISample>();
+
+var container = buider.Build();
+
+HostFactory.Run(x =>
+{
+    var hostConfig = x.UseAutofacContainer(container);
+
+    x.Service<DemoService>(p =>
+    {
+        p.ConstructUsingAutofacContainer();
+        p.WhenStarted(ds => ds.Start());
+        p.WhenStopped(ds => ds.Stop());
+    });
+    x.UseSerilog(Log.Logger);
+    x.RunAsPrompt();
+    x.SetDescription("Demo Service using .net core and TopShelf");
+    x.SetDisplayName("Demo Service");
+    x.SetServiceName("DemoService");
+
+});
